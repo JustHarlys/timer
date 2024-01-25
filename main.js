9b05223071ad0a484bd3b39bd3c2ae2cda77430a
harly <harlysmc@gmail.com>
2024-01-25 11:28:01 -0400

    Timer V2.0

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
-let minutesLabel = document.getElementById("minutes");
-let secondsLabel = document.getElementById("seconds");
-let hoursLabel = document.getElementById("hours");
-let totalSeconds = 0;
-setInterval(setTime, 1000);
+let cronometroID;
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
+let cronometroActivo = false;
 
-function setTime() {
-    ++totalSeconds;
-    secondsLabel.innerHTML = pad(totalSeconds % 60);
-    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60) % 60);
-    hoursLabel.innerHTML = pad(parseInt(totalSeconds / 3600));
-}
-
-function pad(val){
-    let valString = val + "";
-    if (valString.length < 2) {
-        return "0" + valString;
-    }
-    else {
-        return valString
+function iniciarCronometro() {
+    if (!cronometroActivo) {
+        cronometroID = setInterval(actualizarCronometro, 1000);
+        cronometroActivo = true;
     }
 }
+
+function detenerCronometro() {
+    clearInterval(cronometroID);
+    cronometroActivo = false;
+}
+
+function actualizarCronometro() {
+    segundos++;
+    if (segundos >= 60) {
+        segundos = 0;
+        minutos++;
+        if (minutos >= 60) {
+            minutos = 0;
+            horas++;
+        }
+    }
+    const tiempoFormateado = `${(horas < 10) ? "0" + horas : horas}:${(minutos < 10) ? "0" + minutos : minutos}:${(segundos < 10) ? "0" + segundos : segundos}`;
+    document.getElementById('cronometro').innerText = tiempoFormateado;
+}
+
+document.getElementById('boton').addEventListener('click', function() {
+    if (cronometroActivo) {
+        detenerCronometro();
+    } else {
+        iniciarCronometro();
+    }
+});
